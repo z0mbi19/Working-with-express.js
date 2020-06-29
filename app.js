3a3aaee1d035aead3d67455621d48fe56722b103
z0mbi19 <32523746+z0mbi19@users.noreply.github.com>
2020-06-29 16:12:05 -0300

    Styling our pages and serving files statically

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -7,13 +9,14 @@ const shopRouter = require("./routes/shop");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
 
 app.use(shopRouter);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,10 @@
+const path = require("path");
 const express = require("express");
 
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='/admin/product' method='POST' ><input type='text' name='title' ></input> <button type='submit' >Add Product</button> </form>"
-  );
+  res.sendFile(path.join(__dirname, "..", "views", "add-product.html"));
 });
 
 router.post("/product", (req, res, next) => {
