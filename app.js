5a612c4da83ad16cca1eb6ad5f6c9f884bb79f69
z0mbi19 <gaver19@hotmail.com>
2020-06-30 09:24:26 -0300

    Finishing the Pug template

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRouter);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404");
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -6,11 +6,15 @@ const router = express.Router();
 const products = [];
 
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "..", "views", "add-product.html"));
+  res.render("add-product", {
+    PageTitle: "Add Product",
+    path: "/admin/add-product",
+  });
 });
 
 router.post("/product", (req, res, next) => {
   products.push({ title: req.body.title });
+  console.log(products);
   res.redirect("/");
 });
 
