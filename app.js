0fb7a7a17ca1c0be338730e583741b9e63e69936
z0mbi19 <gaver19@hotmail.com>
2020-07-02 08:06:01 -0300

    Working with handlebars

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
 
 const adminData = require("./routes/admin");
 const shopRouter = require("./routes/shop");
 
 const app = express();
 
-app.set("view engine", "pug");
+app.engine("hbs", expressHbs());
+app.set("view engine", "hbs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -18,7 +20,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRouter);
 
 app.use((req, res, next) => {
-  res.status(404).render("404");
+  res.status(404).render("404", { pageTitle: "404" });
 });
 
 app.listen(3000);
