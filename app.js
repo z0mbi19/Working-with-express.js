e1ed0a2b6218607d41a4c8edfb9f909bdfc5ae04
z0mbi19 <gaver19@hotmail.com>
2020-07-02 15:45:20 -0300

    Working on the layout with partials

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRouter);
 
 app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "404" });
+  res.status(404).render("404", { pageTitle: "404", path: "404" });
 });
 
 app.listen(3000);
