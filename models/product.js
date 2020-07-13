b65c955434afa0fa160b7b5da18f9b8a03dc0b9c
z0mbi19 <gaver19@hotmail.com>
2020-07-13 10:32:35 -0300

    Storing product data

--- models/product.js
+++ models/product.js
@@ -18,8 +18,11 @@ const getProductsFromFIle = (cb) => {
 };
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageURL, description, price) {
+    this.title = title;
+    this.imageURL = imageURL;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
